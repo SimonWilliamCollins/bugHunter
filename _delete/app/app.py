f33c37451f9711b8a062274bedccfd2c56ef05eb
Simon Collins <simon.collins287@gmail.com>
2021-01-03 16:58:42 +1030

    removed venv files

--- _delete/app/app.py
+++ _delete/app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    name = 'Rosalia'
-    return render_template('index.html',title='Welcome', username=name)
+    users = ['Helen', 'Adrianna', 'Victoria']
+    return render_template('index.html',title='Welcome', members=users)
 
 app.run(host='0.0.0.0', port=81)
