8be1dc80555e695c7c1b49b29f93f220d782fd58
Simon Collins <simon.collins287@gmail.com>
2021-01-03 18:10:50 +1030

    completed route tut on flask

--- _delete/app/app.py
+++ _delete/app/app.py
@@ -1,12 +1,47 @@
 from flask import render_template
 from flask import Flask
+from flask import request
+
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
+@app.route('/dashboard/<name>')
+def dashboard(name):
+    return 'Welcome %s' % name
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['name']
+        return redirect(url_for('dashboard', name = user))
+    else:
+        user = request.args.get('name')
+        return render_template('login.html')
+
 @app.route('/')
-@app.route('/index')
+@app.route('/index/<name>')
 def index():
     users = ['Helen', 'Adrianna', 'Victoria']
     return render_template('index.html',title='Welcome', members=users)
 
+@app.route('/hello')
+def hello_world():
+    return "hello world"
+
+@app.route('/product/<name>')
+def get_product(name):
+    return "The product is " + str(name)
+
+@app.route('/sale/<transaction_id>')
+def get_sale(transaction_id = 0):
+    return "The transaction is " +str(transaction_id)
+
+@app.route('/create/<first_name>/<last_name>')
+def create(first_name=None,last_name=None):
+    return 'hello ' + first_name + ',' + last_name 
+
 app.run(host='0.0.0.0', port=81)
+
+if __name__ == '__main__':
+    app.run(debug=True)
