c3f944833de5f48940cc640222cf3d186654d0e6
Simon Collins <simon.collins287@gmail.com>
2021-01-07 09:10:17 +1030

    moving computer

--- _delete/app/flask_cookies/app.py
+++ _delete/app/flask_cookies/app.py
@@ -8,14 +8,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/setcookie', methods = ['POST', 'GET'])
+app.route('/setcookie', methods = ['POST', 'GET'])
 def setcookie():
-    if request.method == 'POST':
-        user = request.form['nm']
-
-    resp = make_response(render_template('readcookie.html'))
-    resp.set_cookie('userID', user)
-    return resp
+   if request.method == 'POST':
+   user = request.form['nm']
+   
+   resp = Flask.make_response(render_template('readcookie.html'))
+   resp.set_cookie('userID', user)
+   
+   return resp
 
 @app.route('/getcookie')
 def getcookie():
