11d345a940604885069473e8ba77cd41b38d5003
Simon Collins <simon.collins287@gmail.com>
2021-01-12 16:22:29 +1030

    swap pc

--- app/app.py
+++ app/app.py
@@ -1,12 +1,62 @@
 import json
 from flask import Flask, request, jsonify
 from flask.ext.login import (current_user, loginManager, login_user, logout_user, login_required)
-from falsk_
-
-login_manager = loginManager()
-login_manager.init_app(app)
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+
+app.config['MONGOB_SETTING'] = {
+    'db': 'userAccounts',
+    'host': 'localhost',
+    'port': '27017'
+}
+
+app.secret_key = 'rdffH3455'
+db = MongoEngine()
+login_manager = loginManager()
+db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+@login_manager.user_loader
+def load_user(user_id):
+    return User.objects(id=user_id).first()
+@app.route('/login', methods=['POST'])
+def login():
+    info = json.loads(request.data)
+    username = info.get('username', 'guest')
+    password = info.get('password', '')
+    user = user.objects(name=username, password=password).first()
+
+    if users:
+        login_user(user)
+        return jsonify(user.to_json())
+    else:
+        return jsonify({"status": 401,
+                        "reason": "Username or Password Error"})
+@app.route('/logout', method = ['POST'])
+def logout():
+    logout_user()
+    return jsonify(**{'result': 200,
+                        'data': {'message': 'Logout succuss'}})
+@app.route('/user_info', methods=['POST'])
+def user_info():
+    if current_user.is_authenticated:
+        reap = {"result": 200,
+                "data": current_user.to_json()}
+    else:
+        resp = {"result": 401,
+                "data": {"message": "user no login"}}
+
+    return jsonify(**resp)
+
+class User(db.Document):
+    name = db.StringField()
+    password = db.StringField()
+    email = db.StringField()
+    def to_json(self):
+        return {"name": self.name,
+                "email": self.email}
+    def
 
 @app.route('/login', method = ['POST'])
 def login():
